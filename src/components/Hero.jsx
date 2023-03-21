05d830c6d436eb80f4647979a1487687fbb1186c
Tran Xuan Loc <tx.loc@arborsys.de>
2023-03-22 00:18:50 +0700

    update business

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { discount } from '../assets'
+import { discount, robot } from '../assets'
 import styles from '../style'
 import GetStarted from './GetStarted'
 
@@ -14,16 +14,31 @@ const Hero = () => (
       </div>
 
       <div className='betweenCenter w-full '>
-        <h1 className=' flex-1 font-poppins font-semibold ss:leading-[100px] ss:text-[72px] text[52px] text-white '>
+        <h1 className=' flex-1 font-poppins font-semibold ss:leading-[100px] ss:text-[72px] text-[52px] text-white '>
           The Next
           <br className='sm:block hidden' />
           <span className=' text-gradient '> Generration </span>
-          Payment Methor.
         </h1>
         <div className='ss:flex hidden md:mr-4 mr-0'>
           <GetStarted />
         </div>
       </div>
+      <h1 className=' flex-1 font-poppins font-semibold ss:leading-[100px] ss:text-[72px] text-[52px] text-white '>
+        Payment Methor.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine
+        annual percentage rates, annual fees.
+      </p>
+    </div>
+    <div className=' flex-1 flexCenter md:my-0  my-10 relative'>
+      <img src={robot} alt='billing' className='w-[100%] h-[100%] relative z-[5]' />
+      <div className=' absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
+      <div className=' absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient' />
+      <div className=' absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+    </div>
+    <div className=' ss:hidden flexCenter '>
+      <GetStarted />
     </div>
   </section>
 )
