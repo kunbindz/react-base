12af45cf41f18d3f045b4b015fb2fd80c1a6dd99
Tran Xuan Loc <tx.loc@arborsys.de>
2023-03-25 21:59:33 +0700

    final merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import Layout from './components/Layout'
-import Title from './components/Title'
+import styles from './style'
+import { Billing, Bussiness, Navbar, CTA, CardDeal, Clients, Stats, Footer, Hero, Testimonials } from './components'
 
-function App() {
-  return (
-    <>
-      <Title />
-      <Layout />
-    </>
-  )
-}
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    {/* Navbar */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    {/* Hero */}
+    <div className={`${styles.flexStart} bg-primary `}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    <div className={`${styles.flexStart} ${styles.paddingX} bg-primary`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Bussiness />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+)
 
 export default App
